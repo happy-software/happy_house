6a902fb2b4a8fec77f2b6bd45da1e1ea9bf2d06e
Hebron George <hebrontgeorge@gmail.com>
2018-07-15 21:24:01 -0500

    make the home page our app root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'application#hello'
+  root :to => 'static_pages#home'
 end

bc5d0319e78464e8935ca9e1c2ac825d8785e4a1
Hebron George <hebrontgeorge@gmail.com>
2018-07-20 23:46:59 -0500

    sass up the css; introduce bootstrap; created static about, help, and home pages; initial user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root 'static_pages#home'
+
+  # get 'user/new'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/start', to: 'user#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'static_pages#home'
+
+
 end

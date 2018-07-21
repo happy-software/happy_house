fba9c71a7dbb10ea42bddf9ab73d2fd88b73aff4
Hebron George <hebrontgeorge@gmail.com>
2018-07-21 02:35:29 -0500

    initial passing test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'valid user' do
+    let(:user) { User.new(name: 'Hebron George', email: 'me@hebrongeorge.com') }
+
+    it { expect(user.valid?).to eq true }
+  end
 end

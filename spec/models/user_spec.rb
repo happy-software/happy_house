70f4ff9dd2d6786cc67ce6bb0bad7c94ab75bdb0
Hebron George <hebrontgeorge@gmail.com>
2018-07-21 02:41:27 -0500

    validates presence of name and email fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe User, type: :model do
 
     it { expect(user.valid?).to eq true }
   end
+
+  describe 'invalid user' do
+    let(:user) { User.new(name: name, email: email) }
+    let(:name) { 'Hebron George' }
+    let(:email) { 'me@hebrongeorge.com' }
+
+    context 'blank name' do
+      let(:name) { '      ' }
+      it { expect(user.valid?).to eq false }
+    end
+
+    context 'blank email' do
+      let(:email) { '     ' }
+      it { expect(user.valid?).to eq false }
+    end
+  end
 end

bdb72b7abd9f24d3dea62a00e8640755ff1a1e54
Hebron George <hebrontgeorge@gmail.com>
2018-07-26 23:51:05 -0500

    don't show all users by using except in routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  get    '/users',   to: 'static_pages#home'
-  resources :users
+  resources :users, except: [:index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

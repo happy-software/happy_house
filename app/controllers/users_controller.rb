0c6e57392031af2cea81c679529f5f94a38a5263
Hebron George <hebrontgeorge@gmail.com>
2018-07-28 14:29:32 -0500

    redirect to user home when trying to access other users home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
     end
 
     def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      @user = User.find_by(id: params[:id])
+      redirect_to(current_user) unless current_user?(@user)
     end
 end

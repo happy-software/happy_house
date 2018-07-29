c1afc23535f41a52d5cc6e99570c36e1d86f11d6
Hebron George <hebrontgeorge@gmail.com>
2018-07-29 13:31:09 -0500

    password reset is code complete

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user,   only: [:edit, :update]
-  before_action :valid_user, only: [:edit, :update]
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -23,6 +24,19 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+    password              = params[:user][:password]
+    password_confirmation = params[:user][:password_confirmation]
+
+    if password.empty? || password_confirmation.empty?
+      @user.errors.add(:password, "Can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in(@user)
+      flash[:success] = 'Your password has been reset!'
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -33,5 +47,16 @@ class PasswordResetsController < ApplicationController
 
     def valid_user
       redirect_to root_url unless @user && @user.authenticated?(:reset, params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = 'This password reset link has expired. Try requesting another password reset email.'
+        redirect_to new_password_reset_url
+      end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,10 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
   def downcase_email

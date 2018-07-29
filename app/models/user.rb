be4915748015b160b0e327e251aa81d4d81e583f
Hebron George <hebrontgeorge@gmail.com>
2018-07-29 01:07:45 -0500

    password reset digest and token; send email for password reset helper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_create :create_activation_digest
   before_save   :downcase_email
@@ -46,6 +46,16 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Creates password reset digest
+  def create_reset_digest
+    self.reset_token  = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private

91aa13974b96d1f0dc33beeac7a000ca057a9662
Hebron George <hebrontgeorge@gmail.com>
2018-07-28 20:25:05 -0500

    routes for password reset forms

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   resources :users, except: [:index, :destroy]
   resources :account_activations, only: [:edit]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end

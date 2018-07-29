299a00947eef16af6f2b02af35aa2d3dc06a9eab
Hebron George <hebrontgeorge@gmail.com>
2018-07-28 23:56:46 -0500

    single quote it up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
     else
       render 'new'

46b7987fa989aea8fab1747d5cd872aaff9479a9
Hebron George <hebrontgeorge@gmail.com>
2018-07-29 01:07:54 -0500

    password reset tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -18,16 +18,21 @@ RSpec.describe UserMailer, type: :mailer do
   end
 
   describe 'password_reset' do
-    let(:mail) { UserMailer.password_reset }
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { UserMailer.password_reset(user) }
+
+    before { user.reset_token = User.new_token}
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Password reset')
-      expect(mail.to).to eq(['to@example.org'])
+      expect(mail.subject).to eq('Happy House - Reset Your Password')
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(['noreply@my-happy-house.herokuapp.com'])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.body.encoded).to include(user.reset_token)
+      expect(mail.body.encoded).to include(edit_password_reset_url(user.reset_token, email: user.email))
+      expect(mail.body.encoded).to include('Reset My Password')
     end
   end
 

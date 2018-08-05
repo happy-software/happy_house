131e5f222458f5f53e780d1e992ad1aa219c770c
Hebron George <hebrontgeorge@gmail.com>
2018-08-05 00:41:05 -0500

    wip; split up address into an hstore; started new view for properties; migrations for properties

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :users, except: [:index, :destroy]
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :properties,          only: [:new, :create, :edit, :update]
 end

07a1a78063855ea2a3ec617f25761f6661e1beeb
Hebron George <hebrontgeorge@gmail.com>
2018-08-06 22:30:57 -0500

    validate property type and some clean up

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,7 @@
 class Property < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :property_type
   PROPERTY_TYPES = [:townhome, :single_family_home, :apartment, :condo, :commercial]
+  validates :property_type, presence: true
+  validates_inclusion_of :property_type, :in => PROPERTY_TYPES.map(&:to_s).map(&:titleize)
 end

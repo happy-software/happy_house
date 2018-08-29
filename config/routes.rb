85c684fb4c2f1c05f8ca4698ccffab4e82e3c0da
Hebron George <hebrontgeorge@gmail.com>
2018-08-28 21:09:01 -0500

    adding the index and show actions to routes for Property

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users, except: [:index, :destroy]
+  resources :users,               except: [:index, :destroy]
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :properties,          only: [:new, :create, :edit, :update]
+  resources :properties,          only: [:new, :create, :edit, :update, :index, :show]
 end

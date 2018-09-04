53f73fca3d87ce93c48909f0a2749bd313951808
Hebron George <hebrontgeorge@gmail.com>
2018-09-03 20:17:34 -0500

    property document type migration

---
+++ db/migrate/20180902203713_add_property_document_type_to_property_document.rb
@@ -0,0 +1,5 @@
+class AddPropertyDocumentTypeToPropertyDocument < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :property_documents, :property_document_type, index: true
+  end
+end
---
+++ spec/factories/property_document_types.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :property_document_type do
+    name "MyString"
+  end
+end

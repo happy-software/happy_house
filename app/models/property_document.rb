082d44e0d702c34d673f2ebbd712f4f42db38526
Hebron George <hebrontgeorge@gmail.com>
2018-09-03 20:14:32 -0500

    set up property document type association

--- app/models/property_document.rb
+++ app/models/property_document.rb
@@ -1,5 +1,7 @@
 class PropertyDocument < ApplicationRecord
-  attr_accessor :type, :name, :document
   belongs_to :property
+  belongs_to :property_document_type
   has_one_attached :document
+
+  accepts_nested_attributes_for :property_document_type, allow_destroy: false
 end

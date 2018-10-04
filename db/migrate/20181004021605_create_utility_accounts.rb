4820089079b9c320d70ab2f7a00fc95993701c71
Hebron George <hebrontgeorge@gmail.com>
2018-10-03 21:46:38 -0500

    added utility accounts that can store details on the utility's account; e.g. electricity login/pass/url

---
+++ db/migrate/20181004021605_create_utility_accounts.rb
@@ -0,0 +1,11 @@
+class CreateUtilityAccounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :utility_accounts do |t|
+      t.string :name
+      t.jsonb :details
+      t.references :property, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/utility_accounts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :utility_account do
+    name "MyString"
+    details ""
+    property nil
+  end
+end

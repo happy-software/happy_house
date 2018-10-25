38eb83b6f160570ce625885b9808818ea949408a
Hebron George <hebrontgeorge@gmail.com>
2018-10-24 21:29:43 -0500

    Return the display name of the property along with the rest of it's attributes

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -2,7 +2,8 @@ module Api::V1
   class PropertiesController < ApplicationController
     def index
       @properties = Property.all
-      render json: @properties
+      properties_to_return = @properties.map { |p| p.as_json.merge(display_name: p.display_name) }
+      render json: properties_to_return
     end
   end
 end

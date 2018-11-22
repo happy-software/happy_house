bdebc637bcd07e833885f20ff308ac9f8dc9f75a
Hebron George <hebrontgeorge@gmail.com>
2018-11-21 23:59:16 -0600

    made a create action to receive year

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -2,4 +2,11 @@ class ExpenseReportsController < ApplicationController
   def index
     @property = Property.find(params[:id])
   end
+
+  def create
+    @property = Property.find(params[:id])
+    @expense_report = @property.property_interface.build_expense_report(year: params[:expense_year])
+
+    # TODO: build create view
+  end
 end

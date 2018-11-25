908d322b5898348a7feb942b582adefbf184437e
Hebron George <hebrontgeorge@gmail.com>
2018-11-25 17:44:53 -0600

    require name, cost, and expense date for expense items

--- app/models/expense_item.rb
+++ app/models/expense_item.rb
@@ -1,3 +1,7 @@
 class ExpenseItem < ApplicationRecord
   belongs_to :property
+
+  validates :name,  presence: true
+  validates :cost,  presence: true
+  validates :expense_date, presence: true
 end

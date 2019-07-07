0c8616586e8b860c08f397993da6f0b165e57ac2
Hector Rios <that.hector@gmail.com>
2019-07-07 10:55:06 -0500

    Eager load contracts in #index page

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,37 +1,37 @@
 class LeasesController < ApplicationController
   # before_action :correct_user,   only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  before_action :set_property
   before_action :set_lease, only: [:show, :edit, :update, :destroy]
 
-  # GET /leases
-  # GET /leases.json
+  # GET /properties/:property_id/leases
+  # GET /properties/:property_id/leases.json
   def index
-    properties = current_user&.properties
-    @leases = properties&.flat_map(&:leases)
+    @leases = @property.leases.with_eager_loaded_contract
   end
 
-  # GET /leases/1
-  # GET /leases/1.json
+  # GET /properties/:property_id/leases/1
+  # GET /properties/:property_id/leases/1.json
   def show
   end
 
-  # GET /leases/new
+  # GET /properties/:property_id/leases/new
   def new
     @lease = Lease.new
   end
 
-  # GET /leases/1/edit
+  # GET /properties/:property_id/leases/1/edit
   def edit
   end
 
   # POST /leases
   # POST /leases.json
   def create
-    @lease = Lease.new(lease_params)
+    @lease = @property.leases.new(lease_params)
 
     respond_to do |format|
       if @lease.save
-        format.html { redirect_to @lease, notice: 'Lease was successfully created.' }
-        format.json { render :show, status: :created, location: @lease }
+        format.html { redirect_to [@property, @lease], notice: 'Lease was successfully created.' }
+        format.json { render :show, status: :created, location: [@property, @lease] }
       else
         format.html { render :new }
         format.json { render json: @lease.errors, status: :unprocessable_entity }
@@ -39,13 +39,13 @@ class LeasesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /leases/1
-  # PATCH/PUT /leases/1.json
+  # PATCH/PUT /properties/:property_id/leases/1
+  # PATCH/PUT /properties/:property_id/leases/1.json
   def update
     respond_to do |format|
       if @lease.update(lease_params)
-        format.html { redirect_to @lease, notice: 'Lease was successfully updated.' }
-        format.json { render :show, status: :ok, location: @lease }
+        format.html { redirect_to [@property, @lease], notice: 'Lease was successfully updated.' }
+        format.json { render :show, status: :ok, location: [@property, @lease] }
       else
         format.html { render :edit }
         format.json { render json: @lease.errors, status: :unprocessable_entity }
@@ -53,12 +53,12 @@ class LeasesController < ApplicationController
     end
   end
 
-  # DELETE /leases/1
-  # DELETE /leases/1.json
+  # DELETE /properties/:property_id/leases/1
+  # DELETE /properties/:property_id/leases/1.json
   def destroy
     @lease.destroy
     respond_to do |format|
-      format.html { redirect_to leases_url, notice: 'Lease was successfully destroyed.' }
+      format.html { redirect_to property_leases_url, notice: 'Lease was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -69,17 +69,16 @@ class LeasesController < ApplicationController
       @lease = Lease.find(params[:id])
     end
 
+    def set_property
+      @property = Property.find(params[:property_id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def lease_params
       params.require(:lease).permit(:start_date,
                                     :end_date,
                                     :amount,
+                                    :lease_frequency_id,
                                     :contract)
     end
-
-    # def correct_user
-    #   leases = @leases
-    #
-    #   redirect_to root_url unless current_user&.properties.include?()
-    # end
 end
--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,5 +1,6 @@
 class Lease < ApplicationRecord
   belongs_to :property
+  belongs_to :lease_frequency
 
   has_one_attached :contract
   has_many :lease_tenants
@@ -7,9 +8,10 @@ class Lease < ApplicationRecord
 
   accepts_nested_attributes_for :lease_tenants
 
-  # belongs_to :property_document
-  # has_one :property, through: :property_document
-  # belongs_to :lease_frequency
+  # Ensure that we are able to load contracts that have an attached contract
+  # in a single query, as opposed to n+1 queries
+  # https://semaphoreci.com/blog/2017/08/09/faster-rails-eliminating-n-plus-one-queries.html
+  scope :with_eager_loaded_contract, -> { eager_load(contract_attachment: :blob) }
 
   def expired?(date=nil)
     date = date || DateTime.now

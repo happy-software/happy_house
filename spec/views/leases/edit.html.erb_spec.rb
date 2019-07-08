dfe3f6e012f015ab322a272f693f82b3934c5431
Hector Rios <that.hector@gmail.com>
2019-07-08 13:40:58 -0500

    Fix broken view specs

--- spec/views/leases/edit.html.erb_spec.rb
+++ spec/views/leases/edit.html.erb_spec.rb
@@ -3,22 +3,18 @@ require 'rails_helper'
 RSpec.describe "leases/edit", type: :view do
   let(:user) { create(:user) }
   let(:property) { create(:property, user: user) }
-  # let(:property) { user.properties.create!(property_type: Property::PROPERTY_TYPES.sample.to_s.titleize) }
   let(:lease_frequency) { LeaseFrequency.create(frequency: 'every_month') }
   let(:lease) { Lease.create!(property_id: property.id, lease_frequency_id: lease_frequency.id, details: '') }
 
-  before(:each) do
-    @property = assign(:property, property)
-    @lease = assign(:lease, lease)
+  before do
+    assign(:property, property)
+    assign(:lease, lease)
   end
 
   it "renders the edit lease form" do
     render
 
-    assert_select "form[action=?][method=?]", property_lease_path(@property, @lease), "post" do
-
-      # assert_select "input[name=?]", "lease[details]"
-
+    assert_select "form[action=?][method=?]", property_lease_path(property, lease), "post" do
       assert_select "input[name=?]", "lease[contract]"
     end
   end

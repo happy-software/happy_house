4000a54705f40982f26b346a39dc98fb2ec0d825
Hebron George <hebrontgeorge@gmail.com>
2019-07-18 22:43:24 -0500

    Create schedule with a hello world worker; Update config to not point to redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+Sidekiq.configure_server do |_config|
   schedule_file = 'config/schedule.yml'
 
   if File.exists?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash(YAML.load_file(schedule_file))
   end
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
 end

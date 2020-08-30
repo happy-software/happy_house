98c8e814397b37909b18205f44387aa55ca5ad61
Hebron George <hebrontgeorge@gmail.com>
2020-08-30 12:14:37 -0500

    Clean up syntax

--- app/models/expense_item/one_off_uploaders.rb
+++ app/models/expense_item/one_off_uploaders.rb
@@ -9,13 +9,7 @@ module OneOffUploaders
       cost = amount
       expense_date = DateTime.parse("#{year}-#{month}-01")
 
-      ExpenseItem.new.tap do |item|
-        item.name = name
-        item.cost = cost
-        item.expense_date = expense_date
-        item.property = property
-        item.save!
-      end
+      ExpenseItem.create!(name: name, cost: cost, expense_date: expense_date, property: property)
     end
   end
 
@@ -25,13 +19,7 @@ module OneOffUploaders
       cost = amount
       expense_date = DateTime.parse("#{year}-#{month}-01")
 
-      ExpenseItem.new.tap do |item|
-        item.name = name
-        item.cost = cost
-        item.expense_date = expense_date
-        item.property = property
-        item.save!
-      end
+      ExpenseItem.create!(name: name, cost: cost, expense_date: expense_date, property: property)
     end
   end
 end

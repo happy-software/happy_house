689dc07c1812db5409de4c6e837465ddb9a04383
Hebron George <hebrontgeorge@gmail.com>
2020-08-31 21:34:53 -0500

    rails app:update

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,10 @@
+begin
+  load File.expand_path("../bin/spring", __dir__)
+rescue LoadError => e
+  raise unless e.path.end_with?("/bin/spring")
+end
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.

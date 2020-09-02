d9509daa052c65f3a35743fbf3fd202eb6a2dfbf
Hebron George <hebrontgeorge@gmail.com>
2020-09-02 09:11:39 -0500

    Run migration to add foreign key to active_storage_attachments on their blobs

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_153801) do
+ActiveRecord::Schema.define(version: 2020_09_02_140816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 2020_08_30_153801) do
     t.index ["property_id"], name: "index_utility_accounts_on_property_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "expense_items", "properties"
   add_foreign_key "lease_tenants", "leases"

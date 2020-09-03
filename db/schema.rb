4b24e3c72f87ebfa5aa77b612325d1986a01014d
Hebron George <hebrontgeorge@gmail.com>
2020-09-03 18:56:22 -0500

    Rollback before active storage changes came in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_153801) do
+ActiveRecord::Schema.define(version: 2019_07_06_034035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,14 +33,7 @@ ActiveRecord::Schema.define(version: 2020_08_30_153801) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
-    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "expense_items", force: :cascade do |t|
@@ -142,7 +135,6 @@ ActiveRecord::Schema.define(version: 2020_08_30_153801) do
     t.index ["property_id"], name: "index_utility_accounts_on_property_id"
   end
 
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "expense_items", "properties"
   add_foreign_key "lease_tenants", "leases"
   add_foreign_key "lease_tenants", "tenants"

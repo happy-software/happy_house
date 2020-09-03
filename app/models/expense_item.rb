b051bb4e385e4687f752237c64f45d5dd56e9525
Hebron George <hebrontgeorge@gmail.com>
2020-09-03 18:23:51 -0500

    Idk what I was thinking, the error clearly said 'service' was an unrecognized keyword

--- app/models/expense_item.rb
+++ app/models/expense_item.rb
@@ -8,5 +8,5 @@ class ExpenseItem < ApplicationRecord
   validates :cost,  presence: true
   validates :expense_date, presence: true
 
-  has_many_attached :attachments, service: :s3
+  has_many_attached :attachments
 end

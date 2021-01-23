4de8a6389cadae5cd9296256092a2f3a43c3c5f1
Hebron George <hebrontgeorge@gmail.com>
2021-01-22 20:40:28 -0500

    Add a chart summarizing expenses across all years; Add a chart summarizing expenses across months in individual years

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -1,14 +1,19 @@
 class ExpenseReportsController < ApplicationController
+
   before_action :correct_user,   only: [:index, :create]
 
   def index
-    @property = Property.find(params[:id])
+    @property        = Property.find(params[:id])
+    @available_years = @property.property_interface.expense_years
+    @property_name   = @property.display_name
+    @yearly_expense_summary = @property.property_interface.yearly_expense_summary
   end
 
   def create
     @property = Property.find(params[:id])
     @year     = params[:expense_year]
     @expense_report = @property.property_interface.build_expense_report(year: @year)
+    @year_summary   = @property.property_interface.monthly_expense_summary(@year)
   end
 
   private

5a1bb13c37886f76896ab285766d7fea1e45ee22
Hebron George <hebrontgeorge@gmail.com>
2021-01-28 01:16:58 -0500

    Set renew lease url in controller and pass it into the view

--- app/mailers/lease_mailer.rb
+++ app/mailers/lease_mailer.rb
@@ -3,6 +3,7 @@ class LeaseMailer < ApplicationMailer
     @lease    = params[:lease]
     @property = @lease.property
     @user     = @property.user
+    @url      = property_lease_renew_current_lease_url(@property.id, @lease.id, id: @lease.id)
 
     mail(to: @user.email, subject: 'Lease Expiration Reminder')
   end

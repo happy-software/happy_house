84dbc6e3b01358b65790d51e9ee83039889d4871
Hebron George <hebrontgeorge@gmail.com>
2021-01-30 22:37:37 -0500

    revert it lol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ Rails.application.configure do
 
   # https://sentry.io/organizations/my-happy-house/projects/my-happy-house/getting-started/ruby-rails/
   Sentry.init do |config|
-    config.dsn = ENV['SENTRY_DSN_V2']
+    config.dsn = ENV['SENTRY_DSN']
     config.breadcrumbs_logger = [:active_support_logger]
 
     config.traces_sample_rate = 0.5

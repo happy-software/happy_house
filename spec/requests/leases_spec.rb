0e708c7a24817c4fa2cd4378172cf93bb4496db2
Hebron George <hebrontgeorge@gmail.com>
2021-08-09 19:33:31 -0400

    Log in user first before request the property's leases

--- spec/requests/leases_spec.rb
+++ spec/requests/leases_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Leases", type: :request do
-  let(:mock_property) { Property.new(id: 1) }
+  let(:mock_user)     { User.new(id: 1, password: 'password', email: 'test@test.com', activated: true) }
+  let(:mock_property) { Property.new(id: 1, user: mock_user) }
 
   before do
+    allow(User).to receive(:find_by).and_return(mock_user)
     allow(Property).to receive(:find).and_return(mock_property)
   end
 
   describe "GET /leases" do
     it "works! (now write some real specs)" do
-      get property_leases_path(property_id: mock_property.id)
+      post login_path, params: {"session": {"email": mock_user.email, "password": mock_user.password}}
+      get user_property_leases_path(user_id: mock_user.id, property_id: mock_property.id)
       expect(response).to have_http_status(200)
     end
   end

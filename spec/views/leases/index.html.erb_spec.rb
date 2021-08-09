80dd48a57e06b7b80b0a7723a0d30d06da39472c
Hebron George <hebrontgeorge@gmail.com>
2021-08-09 19:55:26 -0400

    Assign current_user so the views can populate paths correctly

--- spec/views/leases/index.html.erb_spec.rb
+++ spec/views/leases/index.html.erb_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "leases/index", type: :view do
-  let(:user) { create(:user) }
-  let(:property) { create(:property, user: user) }
+  let(:user)            { create(:user) }
+  let(:property)        { create(:property, user: user) }
   let(:lease_frequency) { create(:lease_frequency) }
-  let(:first_lease) { Lease.create!(property_id: property.id, lease_frequency_id: lease_frequency.id, details: '') }
-  let(:second_lease) { Lease.create!(property_id: property.id, lease_frequency_id: lease_frequency.id, details: '') }
+  let(:first_lease)     { Lease.create!(property_id: property.id, lease_frequency_id: lease_frequency.id, details: '') }
+  let(:second_lease)    { Lease.create!(property_id: property.id, lease_frequency_id: lease_frequency.id, details: '') }
   
   let(:fake_attachment) { fixture_file_upload(Rails.root.join('public', 'apple-touch-icon.png'), 'image/png') }
 
@@ -15,6 +15,7 @@ RSpec.describe "leases/index", type: :view do
     second_lease.contract.attach(fake_attachment)
     assign(:property, property)
     assign(:leases, [first_lease, second_lease])
+    assign(:current_user, user)
   end
 
   it "renders a list of leases" do

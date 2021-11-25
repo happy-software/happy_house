3bc0a04e816be1fa6b19b58ece4de3e8fa4de1e7
Hebron George <hebrontgeorge@gmail.com>
2021-11-25 09:48:31 -0500

    Remove dupe commands in puma config

--- config/puma.rb
+++ config/puma.rb
@@ -40,8 +40,6 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+

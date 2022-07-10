dfb22094d19e0c89d34f77ed69a0d907e60693a1
Hebron George <hebrontgeorge@gmail.com>
2022-07-09 21:00:19 -0400

    Swap Sendgrid for Mailgun

--- app/mailers/lease_mailer.rb
+++ app/mailers/lease_mailer.rb
@@ -5,7 +5,7 @@ class LeaseMailer < ApplicationMailer
     @lease    = params[:lease]
     @property = @lease.property
     @user     = @property.user
-    @url      = property_lease_renew_current_lease_url(@property.id, @lease.id, id: @lease.id)
+    @url      = user_property_lease_renew_current_lease_url(@user.id, @property.id, @lease.id, id: @lease.id)
 
     mail(to: @user.email, subject: "Lease Expiration Reminder")
   end

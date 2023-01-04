e9d4e3e1921e4e8ae0747e3b44faafd72633fe3d
Hebron George <hebrontgeorge@gmail.com>
2023-01-03 21:07:10 -0500

    Update gems

--- config/puma.rb
+++ config/puma.rb
@@ -41,8 +41,6 @@ workers ENV.fetch("WEB_CONCURRENCY", 2)
 #
 preload_app!
 
-rackup DefaultRackup
-
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/

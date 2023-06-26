80beb054a15690f1c54d846ae971957ce2f67833
Hebron George <hebrontgeorge@gmail.com>
2023-06-26 11:59:56 -0400

    Update Lease Expiration Reminder Email to use new Renewal Flow

--- app/mailers/lease_mailer.rb
+++ app/mailers/lease_mailer.rb
@@ -5,7 +5,7 @@ class LeaseMailer < ApplicationMailer
     @lease    = params[:lease]
     @property = @lease.property
     @user     = @property.user
-    @url      = user_property_lease_renew_current_lease_url(@user.id, @property.id, @lease.id, id: @lease.id)
+    @url      = user_property_lease_new_renewal_url(@user.id, @property.id, @lease.id, lease_id: @lease.id)
 
     mail(to: @user.email, subject: "Lease Expiration Reminder")
   end

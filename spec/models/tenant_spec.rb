3311791c1918d27151f0bc08882380dc984aa819
Hebron George <hebrontgeorge@gmail.com>
2024-01-07 16:35:42 -0500

    Simple spec for tenant model

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -3,5 +3,36 @@
 require "rails_helper"
 
 RSpec.describe Tenant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tenant)    { FactoryBot.create(:tenant) }
+  let(:property1) { FactoryBot.create(:property, user: user) }
+  let(:property2) { FactoryBot.create(:property, user: user) }
+  let(:user)      { FactoryBot.create(:user) }
+  let(:lease1)    { FactoryBot.create(:lease, start_date: 2.years.ago, end_date: 1.year.ago, property: property1) }
+  let(:lease2)    { FactoryBot.create(:lease, start_date: 1.day.ago, end_date: 1.year.from_now, property: property2) }
+
+  describe '#current_leases' do
+    before do
+      lease1
+      lease2
+      FactoryBot.create(:lease_tenant, lease: lease1, tenant: tenant)
+      FactoryBot.create(:lease_tenant, lease: lease2, tenant: tenant)
+    end
+
+    it 'should return just lease2' do
+      expect(tenant.current_leases).to match_array([lease2])
+    end
+  end
+
+  describe '#current_properties' do
+    before do
+      lease1
+      lease2
+      FactoryBot.create(:lease_tenant, lease: lease1, tenant: tenant)
+      FactoryBot.create(:lease_tenant, lease: lease2, tenant: tenant)
+    end
+
+    it 'should return the current property' do
+      expect(tenant.current_properties).to match_array([property2])
+    end
+  end
 end
